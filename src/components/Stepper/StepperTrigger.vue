f378775a55f0074d5d5eb63d4103759010e2b458
rushabh57 <rushabhmalvaniya12@gmail.com>
2026-03-04 19:41:11 +0530

    stepper tweaks

--- src/components/Stepper/StepperTrigger.vue
+++ src/components/Stepper/StepperTrigger.vue
@@ -31,4 +31,4 @@ import { StepperTrigger as RekaStepperTrigger } from "reka-ui";
 </template>
 
 
-<!-- git remote add  https://github.com/strangekit/vueon-ui.git -->
+<!-- demo cmd: git remote add  https://github.com/strangekit/vueon-ui.git -->
